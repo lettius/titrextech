f4a31264a53432457fb88dbc53bb824b7dd0f7a0
lettius93 <lettiusares93@gmail.com>
2022-10-15 10:01:33 +0300

    .

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -5,8 +5,8 @@
       <div class="row no-gutters">
         <div class="content col-xl-6 d-flex align-items-stretch">
           <div class="content">
-            <h3>Titrex Technology</h3>
-            <p>
+            <h4>Our company</h4>
+            <p class="text-muted">
               Company deal with system development including web design,web development and mobile development,
               <br>Security system such as CCTV System installation and configuration and Data and Voice installation and configuration.
             </p>
@@ -15,13 +15,13 @@
         <div class="col-xl-6 d-flex align-items-stretch">
           <div class="icon-boxes d-flex flex-column justify-content-center">
             <div class="row">
-              <div class="opacity col-md-6 icon-box" data-aos="fade-up" data-aos-delay="100" style="">
+              <div class="col-md-6 icon-box" data-aos="fade-up" data-aos-delay="100" style="">
                 <h4>Mission</h4>
-                <p>Delivering innovative and reliable solutions to meet our clients needs with utmost quality and perfection throughout development</p>
+                <p class="text-muted">Delivering innovative and reliable solutions to meet our clients needs with utmost quality and perfection throughout development</p>
               </div>
-              <div class="opacity col-md-6 icon-box" data-aos="fade-up" data-aos-delay="200">
+              <div class="col-md-6 icon-box" data-aos="fade-up" data-aos-delay="200">
                 <h4>Vision</h4>
-                <p>To become an integrated ICT Solution provider in both deliverancy and ethics</p>
+                <p class="text-muted">To become an integrated ICT Solution provider in both deliverancy and ethics</p>
               </div>
             </div>
           </div><!-- End .content-->
